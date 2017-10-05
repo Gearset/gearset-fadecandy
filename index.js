42fa24c4e5a4af7dec48c0c2077cb34a418369a6
olane <oli@gearset.com>
2017-10-05 17:37:22 +0100

    Const the consts

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 
-var OPC = require('./opc')
-var client = new OPC(process.env.FADECANDY_SERVER || 'localhost', 7890);
-var model = OPC.loadModel(__dirname + '/model.json');
+const OPC = require('./opc');
 const _ = require('underscore');
+
+const client = new OPC(process.env.FADECANDY_SERVER || 'localhost', 7890);
+const model = OPC.loadModel(__dirname + '/model.json');
 
 let black = [0, 0, 0];
 let red = [128, 0, 0];
