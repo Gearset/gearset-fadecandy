1c54bbf608780737a3da595cbe5747a893f596ad
olane <oli@gearset.com>
2017-10-05 17:29:11 +0100

    Make some basic data structures for states, state colours and org states, and remove the pulsing

--- index.js
+++ index.js
@@ -4,24 +4,35 @@ var OPC = require('./opc')
 var client = new OPC(process.env.FADECANDY_SERVER || 'localhost', 7890);
 var model = OPC.loadModel(__dirname + '/model.json');
 
+let black = [0, 0, 0];
+let red = [255, 0, 0];
+let green = [0, 255, 0];
+
+let states = {
+    passing: 'passing',
+    failing: 'failing'
+}
+
+let stateColours = {};
+stateColours[states.passing] = green;
+stateColours[states.failing] = red;
+
 let orgStates = [
-    [255, 0, 0],
-    [0, 255, 0],
-    [0, 0, 255],
-    [255, 255, 255]
+    states.passing,
+    states.failing,
+    states.failing,
+    states.passing
 ]
 
-let black = [0, 0, 0];
+function getOrgColour(orgIndex){
+    let orgState = orgStates[orgIndex];
+    let orgColour = stateColours[orgState];
+    return orgColour;
+}
 
 function draw() {
-    var periods = Math.floor(new Date().getTime() / 100);
-
     client.mapPixels(modelPoint => {
-        if((modelPoint.pixelIndex + periods) % 4 === 0){
-            return black;
-        } else{
-            return orgStates[modelPoint.orgIndex];
-        }
+        return getOrgColour(modelPoint.orgIndex);
     }, model);
 }
 
