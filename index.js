2cfd8a9d628025db0a0081985a17e5c0f19489c5
olane <oli@gearset.com>
2017-10-05 17:36:57 +0100

    Reduce brightness for testing

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ var client = new OPC(process.env.FADECANDY_SERVER || 'localhost', 7890);
 var model = OPC.loadModel(__dirname + '/model.json');
 
 let black = [0, 0, 0];
-let red = [255, 0, 0];
-let green = [0, 255, 0];
+let red = [128, 0, 0];
+let green = [0, 128, 0];
 
 let states = {
     passing: 'passing',
