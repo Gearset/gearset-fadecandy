d20557e2777114b830db8666a747dd346e7ffaeb
olane <oli@gearset.com>
2017-10-06 14:08:15 +0100

    Poll every 2 minutes by default, or specify period in config

--- index.js
+++ index.js
@@ -7,12 +7,15 @@ const stateGetter = require("./orgStateGetter");
 const stateToColourMapping = require('./stateToColourMapping');
 const states = require('./states');
 
+const defaultPollingPeriod = 1000 * 60 * 2; // 2 minutes
+
 const client = new OPC(process.env.FADECANDY_SERVER || 'localhost', 7890);
 const model = OPC.loadModel(__dirname + '/model.json');
 
 const config = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
 const feedUrl = config.feedUrl;
 const projectUrls = config.projectUrls;
+const pollingPeriodMillis = config.pollingPeriodMillis || defaultPollingPeriod;
 
 if(!projectUrls){
     console.error("No project URLS defined in config.json");
@@ -39,22 +42,27 @@ setInterval(draw, 20);
 
 var getter = new stateGetter(feedUrl, parser());
 
-console.log("Getting feed statuses for", projectUrls);
-getter.getParsedFeed().then(
-    (projectsJson) => { 
-        console.log("Got feed statuses");
+function updateOrgStates() {
+    console.log("Getting feed statuses for", projectUrls);
+    return getter.getParsedFeed().then(
+        (projectsJson) => { 
+            console.log("Got feed statuses");
+    
+            orgStates = _.map(projectUrls, projectUrl => {
+                let projectStatus = _.find(projectsJson, x => x.webUrl === projectUrl);
+    
+                if(!projectStatus) {
+                    console.log("No project status returned for project with URL", projectUrl);
+                    return states.unknown;
+                }
+    
+                return projectStatus.status;
+            });
+    
+            console.log("Set org states to", orgStates);
+        }
+    );
+}
 
-        orgStates = _.map(projectUrls, projectUrl => {
-            let projectStatus = _.find(projectsJson, x => x.webUrl === projectUrl);
-
-            if(!projectStatus) {
-                console.log("No project status returned for project with URL", projectUrl);
-                return states.unknown;
-            }
-
-            return projectStatus.status;
-        });
-        
-        console.log("Set org states to", orgStates);
-    }
-);
+updateOrgStates();
+setInterval(updateOrgStates, pollingPeriodMillis)
